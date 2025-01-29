967133c00e07eae9d78a78ce287f7a9933180cf0
malcomcrab <malcomcrabesq@outlook.com>
2025-01-29 15:35:04 +0000

    finished assignment

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
-
+const path = require("node:path");
 const express = require("express");
 const app = express();
-const indexRouter = require("./routes/indexRouter")
-
+app.use(express.urlencoded({ extended: true }));
+const indexRouter = require("./routes/indexRouter");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use("/", indexRouter);
 
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,7 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+
 const messages = [
     {
       text: "Hi there!",
@@ -16,10 +17,24 @@ const messages = [
   ];
 
 
-indexRouter.get("/", (req, res) => res.send("Index"));
-indexRouter.get("/:indexId", (req, res) => {
+indexRouter.get("/", (req, res) => res.render("index", {title: "Mini Message Board", messages: messages}));
+indexRouter.get("/", (req, res) => {
   const { indexId } = req.params;
   res.send(`index ID: ${indexId}`);
 });
 
+indexRouter.get("/new", (req,res) => res.render("form", {title:"form"}))
+
+indexRouter.get("/message/:id", (req,res) => {
+    const message = messages[req.params.id];
+    res.render("message", {title: "message", message: message})
+})
+
+indexRouter.post("/new", (req, res) => {
+    const messageText = req.body.messageText
+    const messageUser = req.body.messageUser
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    
+    res.redirect("/")
+  })
 module.exports = indexRouter;
